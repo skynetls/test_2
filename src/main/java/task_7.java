1f6806625523907d700906313e202153744aec56
skynetls <lyudocheksi@gmail.com>
2018-10-29 00:44:17 +0300

    editing

--- src/main/java/task_7.java
+++ src/main/java/task_7.java
@@ -1,7 +1,7 @@
 public class task_7 {
     public static void main(String[] args) {
         int i = 1;
-        while (i<51) {
+        while (i<=50) {
             System.out.println(i);
                 ++i;
         }
